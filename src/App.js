fda019bc198dbfa418fad3c82da9ce4e44bdff92
eastar-DS <dororo0104@naver.com>
2025-10-20 20:24:58 +0900

    feat(app): 구분자를 기준으로 문자열 분리 및 숫자로 변환하는 extractNumbers 함수 구현

--- src/App.js
+++ src/App.js
@@ -14,16 +14,23 @@ class App {
     if (input.startsWith(CUSTOM_DELIMITER_PREFIX)) {
       const delimiterIndex = input.indexOf('\\\\n');
       const delimiter = input.substring(2,delimiterIndex);
-      const nums = input.substring(delimiterIndex+2);
-      return { delimiter: this.escapeRegex(delimiter), nums }
+      const numbersString = input.substring(delimiterIndex+2);
+      return { delimiter: this.escapeRegex(delimiter), numbersString }
     }
 
-    return { delimiter: DEFAULT_DELIMITER, nums: input };
+    return { delimiter: DEFAULT_DELIMITER, numbersString: input };
   }
 
   escapeRegex = (str) => str.replace(/[.*+?^${}()|[\\]\\\\]/g, '\\\\$&');
 
-
+  extractNumbers(numbersString, delimiter) {
+    const regex = new RegExp(delimiter);
+    return numbersString
+      .split(regex)
+      .filter((string) => string.trim() !== "")
+      .map((string) => parseInt(string.trim(), 10))
+      .filter((num) => !isNaN(num));
+  }
 }
 
 export default App;
