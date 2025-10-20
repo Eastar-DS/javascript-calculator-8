93dbd4559b816a2a257a7125b7ca015040885cff
eastar-DS <dororo0104@naver.com>
2025-10-20 20:18:16 +0900

    feat(app): 문자열에서 구분자를 추출하고 delimiter와 nums를 반환하는 parseInput함수 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { Console } from "@woowacourse/mission-utils";
 
+const DEFAULT_DELIMITER = '[,:]';
+const CUSTOM_DELIMITER_PREFIX = '//';
 
 class App {
   async run() {
@@ -7,6 +9,19 @@ class App {
 
     Console.print(`결과 : ${inputString}`);
   }
+
+  parseInput(input) {
+    if (input.startsWith(CUSTOM_DELIMITER_PREFIX)) {
+      const delimiterIndex = input.indexOf('\\\\n');
+      const delimiter = input.substring(2,delimiterIndex);
+      const nums = input.substring(delimiterIndex+2);
+      return { delimiter: this.escapeRegex(delimiter), nums }
+    }
+
+    return { delimiter: DEFAULT_DELIMITER, nums: input };
+  }
+
+  escapeRegex = (str) => str.replace(/[.*+?^${}()|[\\]\\\\]/g, '\\\\$&');
 
 
 }
