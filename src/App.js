3e34cadc83a6552d3d9652ff2f0d52233e403b2f
eastar-DS <dororo0104@naver.com>
2025-10-20 20:49:51 +0900

    feat(app): 음수입력과 잘못된 커스텀 구분자 형식입력, 빈입력 시 에러처리 구현

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { Console } from "@woowacourse/mission-utils";
 
 const DEFAULT_DELIMITER = '[,:]';
 const CUSTOM_DELIMITER_PREFIX = '//';
+const ERROR_CUSTOM_DELIMITER = '[ERROR] 커스텀 구분자 형식이 올바르지 않습니다.';
+const ERROR_NEGATIVE_NUMBER = '[ERROR] 음수는 허용되지 않습니다.';
 
 class App {
   async run() {
@@ -22,14 +24,19 @@ class App {
 
     const {delimiter, numbersString} = this.parseInput(inputString);
     const numbersArray = this.extractNumbers(numbersString, delimiter);
+    this.validateNumbers(numbersArray);
+
     return numbersArray.reduce((acc, num) => acc + num, 0);
   }
 
   parseInput(inputString) {
     if (inputString.startsWith(CUSTOM_DELIMITER_PREFIX)) {
-      const delimiterIndex = inputString.indexOf('\\\\n');
+      const delimiterIndex = inputString.indexOf('\\n');
+      if (delimiterIndex === -1) {
+        throw new Error(ERROR_CUSTOM_DELIMITER)
+      }
       const delimiter = inputString.substring(2,delimiterIndex);
-      const numbersString = inputString.substring(delimiterIndex+2);
+      const numbersString = inputString.substring(delimiterIndex + 2);
       return { delimiter: this.escapeRegex(delimiter), numbersString }
     }
 
@@ -45,6 +52,13 @@ class App {
       .filter((string) => string.trim() !== "")
       .map((string) => parseInt(string.trim(), 10))
       .filter((num) => !isNaN(num));
+  }
+
+  validateNumbers(numbersArray) {
+    const negativeNumbers = numbersArray.filter((num) => num < 0);
+    if (negativeNumbers.length > 0) {
+      throw new Error(ERROR_NEGATIVE_NUMBER);
+    }
   }
 }
 
